5b8a59d61aefe6e1174baea63d1f28084f676a8e
DuzDao <duzdao.workspace@gmail.com>
2024-12-15 13:33:31 +0700

    Optimize preprocessing

--- data/process_data.py
+++ data/process_data.py
@@ -1,7 +1,7 @@
 import json
 import os
 from utils import flatten_html_table, format_table
-from tqdm import tqdm 
+from tqdm import tqdm
 
 DATA_DIR = "data/raw"
 OUTPUT_DIR = "data/processed"
@@ -18,14 +18,22 @@ def process_data():
     with open(os.path.join(DATA_DIR, "qas_test.json"), "r") as f:
         test_qas = json.load(f)["qas"]
     with open(os.path.join(DATA_DIR, "table.json"), "r") as f:
-        tables = {table["table_id"]: table["table_html"] for table in json.load(f)["table"]}
+        tables = json.load(f)["tables"]
 
-    def _process_qa(qas, tables, name_set):
+    # Pre-process tables
+    processed_tables = {}
+    print("Processing tables...")
+    for table in tqdm(tables, desc="Flattening tables"):
+        table_id = table["table_id"]
+        table_html = table["table_html"]
+        table_flatten = format_table(flatten_html_table(table_html))
+        processed_tables[table_id] = table_flatten
+
+    def _process_qa(qas, processed_tables, name_set):
         processed_data = []
         for qa in tqdm(qas, desc="Processing {} data...".format(name_set)):
             table_id = qa["table_id"]
-            table_html = tables[table_id]
-            table_flatten = format_table(flatten_html_table(table_html))
+            table_flatten = processed_tables[table_id]
             processed_data.append({
                 "question": qa["question"],
                 "table": table_flatten,
@@ -33,9 +41,9 @@ def process_data():
             })
         return processed_data
 
-    train_processed = _process_qa(train_qas, tables, "train")
-    dev_processed = _process_qa(dev_qas, tables, "dev")
-    test_processed = _process_qa(test_qas, tables, "test")
+    train_processed = _process_qa(train_qas, processed_tables, "train")
+    dev_processed = _process_qa(dev_qas, processed_tables, "dev")
+    test_processed = _process_qa(test_qas, processed_tables, "test")
 
     with open(os.path.join(OUTPUT_DIR, "train.json"), "w") as f:
         json.dump(train_processed, f, indent=4)
